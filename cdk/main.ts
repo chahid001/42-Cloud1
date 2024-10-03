116a0527abfe7b253e116f2d4a01b9f46c9ca686
chahid001 <soufianeel288@gmail.com>
2024-10-03 18:32:19 +0100

    finalize test for CDKTF

--- cdk/main.ts
+++ cdk/main.ts
@@ -1,8 +1,12 @@
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
-import { GoogleProvider } from "@cdktf/provider-google/lib/provider";
+import { App, TerraformOutput, TerraformStack } from "cdktf";
+import { provider } from "@cdktf/provider-google";
+import { ComputeNetwork } from "@cdktf/provider-google/lib/compute-network";
 import * as dotenv from "dotenv";
-
+import { ComputeSubnetwork } from "@cdktf/provider-google/lib/compute-subnetwork";
+import { ComputeFirewall } from "@cdktf/provider-google/lib/compute-firewall";
+import { ComputeAddress } from "@cdktf/provider-google/lib/compute-address";
+import { ComputeInstance } from "@cdktf/provider-google/lib/compute-instance";
 
 dotenv.config()
 
@@ -10,11 +14,71 @@ class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    new GoogleProvider(this, "Google", {
+    new provider.GoogleProvider(this, "Google", {
       project: process.env.PROJECT_NB,
       region: "us-central1"
-    })
+    });
 
+    const vpc = new ComputeNetwork(this, "VPC", {
+      name: "cloud-1-vpc"
+    });
+
+    const subnet_app = new ComputeSubnetwork(this, "Subnet-app", {
+      name: "app-cloud-subnet",
+      network: vpc.name,
+      ipCidrRange: "10.0.1.0/24",
+      region: "us-central1"
+    });
+
+    new ComputeFirewall(this, "Firewall_app", {
+      name: "app-cloud-firewall",
+      network: vpc.name,
+
+      allow: [{
+        protocol: "tcp",
+        ports: ["22"]
+      }],
+
+      sourceRanges: ["0.0.0.0/0"]
+
+    });
+
+    const PubIP = new ComputeAddress(this, "PubIP-cloud1", {
+      name: "app-externe-add"
+    }); 
+
+    const vm_app = new ComputeInstance(this, "VM-App", {
+      name: "vm-app-cloud1",
+      machineType: "e2-small",
+      zone: "us-central1-b",
+
+      bootDisk: {
+        initializeParams: {
+          image: "ubuntu-os-cloud/ubuntu-2204-lts",
+          size: 20,
+        },
+      },
+
+      networkInterface: [{
+        network: vpc.name,
+        subnetwork: subnet_app.name,
+        accessConfig: [{
+          natIp: PubIP.address
+        }]
+      }],
+
+      metadata: {
+        "ssh-keys": `cloud1:${process.env.PUBLIC_KEY}`,
+      },
+
+      tags: ["vm-app", "vm1"]
+
+    });
+
+
+    new TerraformOutput(this, "PubIP_Output", {
+      value: vm_app.networkInterface.get(0).accessConfig.get(0).natIp
+    });
 
   }
 }
